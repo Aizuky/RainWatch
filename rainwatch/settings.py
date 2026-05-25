00ebf50c4e048c057114e4928951d43686ced726
Aizuky <almeidaclecio49@gmail.com>
2026-05-25 09:21:01 -0300

    configurando banco em memoria para vercel

--- rainwatch/settings.py
+++ rainwatch/settings.py
@@ -75,12 +75,20 @@ WSGI_APPLICATION = 'rainwatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'VERCEL' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
